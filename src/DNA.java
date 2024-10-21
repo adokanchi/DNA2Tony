3308835562a5d0bfec150d58a42c02c3e1338bf4
adokanchi <tony.dokanchi@menloschool.org>
2024-10-21 11:46:53 -0700

    hello

--- src/DNA.java
+++ src/DNA.java
@@ -14,45 +14,75 @@ public class DNA {
         // Initialize variables
         int seqLen = sequence.length();
         int STRLen = STR.length();
-        boolean[] hasSTR = new boolean[seqLen];
-        long window = 0;
         long STRVal = 0;
-        int[] vals = new int[21];
-        vals['A'-'A'] = 0;
-        vals['C'-'A'] = 1;
-        vals['G'-'A'] = 2;
-        vals['T'-'A'] = 3;
+        long window = 0;
+        int[] vals = new int['t'+1];
+        vals['a'] = 0;
+        vals['c'] = 1;
+        vals['g'] = 2;
+        vals['t'] = 3;
+        vals['A'] = 0;
+        vals['C'] = 1;
+        vals['G'] = 2;
+        vals['T'] = 3;
         for (int i = 0; i < STRLen; i++) {
             STRVal = STRVal << 2;
-            STRVal += vals[Character.toUpperCase(STR.charAt(i)) - 'A'];
+            STRVal += vals[STR.charAt(i)];
         }
         for (int i = 0; i < STRLen; i++) {
             window = window << 2;
-            window += vals[Character.toUpperCase(sequence.charAt(i))-'A'];
+            window += vals[sequence.charAt(i)];
         }
 
-        // hasSTR[i] == true when there is a STR starting from index i
-        // loop through sequence finding where there are STRs
+        // numRepeats[i] is the number of times STR is repeated ending at index i (last STR starts at i)
+        int[] numRepeats = new int[seqLen];
+        int max = 0;
+
+        /*
         for (int i = 0; i <= seqLen - STRLen; i++) {
-            if (window == STRVal) hasSTR[i] = true;
+            if (window == STRVal) {
+                if (i-STRLen >= 0) {
+                    numRepeats[i] = 1 + numRepeats[i-STRLen];
+                }
+                else {
+                    numRepeats[i] = 1;
+                }
+                if (numRepeats[i] > max) max = numRepeats[i];
+            }
             // Shift
             window = window << 2;
-            // IDK why I need this if statement but the chromosome test needs it for some rsn
-            if (sequence.charAt(i + 1) != 0) window += vals[Character.toUpperCase(sequence.charAt(i + 1)) - 'A'];
+            window += vals[sequence.charAt(i + 1)];
+            window = window % (((long) 1) << (2 * STRLen));
+        }
+         */
+
+        for (int i = 0; i <= seqLen - STRLen; i++) {
+            // If the hashes match
+            if (window == STRVal) {
+                // Find numRepeats[i], possibly update max
+                if (i - STRLen >= 0) {
+                    numRepeats[i] = 1 + numRepeats[i - STRLen];
+                }
+                else {
+                    numRepeats[i] = 1;
+                }
+                if (numRepeats[i] > max) max = numRepeats[i];
+                // Shift STRLen times (do STRLen - 1 shifts, one more happens at the end)
+                for (int j = 0; j < STRLen - 1; j++) {
+                    if (i + j  + 1 < seqLen) {
+                        window = window << 2;
+                        window += vals[sequence.charAt(i + j + 1)];
+                        window = window % (((long) 1) << (2 * STRLen));
+                    }
+                }
+                i += STRLen - 1;
+            }
+            // Shift
+            window = window << 2;
+            if (i + 1 < seqLen) window += vals[sequence.charAt(i + 1)];
             window = window % (((long) 1) << (2 * STRLen));
         }
 
-        // numRepeats[i] is the number of times STR is repeated starting from index i
-        int[] numRepeats = new int[seqLen];
-        int max = 0;
-        if (hasSTR[seqLen  - STRLen]) numRepeats[seqLen - STRLen] = 1;
-        // If there are k repeats from index i, there are (k-1) repeats from index i+STRLen
-        for (int i = seqLen - STRLen - 1; i >= 0; i--) {
-            if (hasSTR[i]) {
-                numRepeats[i] = 1 + numRepeats[i+STRLen];
-                if (numRepeats[i] > max) max = numRepeats[i];
-            }
-        }
         return max;
     }
 }
